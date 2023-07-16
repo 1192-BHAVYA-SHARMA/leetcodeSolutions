0c89d8a1e69ffd5f8a961e44e39f96b48386e7f7
BHAVYA SHARMA <120469965+1192-BHAVYA-SHARMA@users.noreply.github.com>
2023-07-16 22:09:32 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -13,42 +13,7 @@ class Solution
     public static void sort012(int a[], int n)
     {
         // code here 
-        
-        /*Dutch national flag algorithm
-        APPROACH: Three pointers --> left,mid,right
-initially left and right are pointed to both the ends of the array respectively
-mid points towards start initially
-program lasts until mid reaches other end of the array
-case 1: if mid encounters -> 0
-        swap arr[mid] , arr[left] 
-        increment left and mid by one/ move both left and 
-        right pointers right side by one
-case 2:if mid encounters -> 1
-    simply increment mid by 1/move mid pointer right by one
-case 3: (last case) if mid encounters -> 2
-swap arr[mid],arr[right]
-decrement only right by one / move right pointer left side by one
-    */
-    int left=0,mid=0,right=n-1;
-    while(mid<=right){
-        if(a[mid]==0){
-            swap(a,mid,left);
-            mid++;
-            left++;
-        }
-        else if(a[mid]==1){
-            mid++;
-        }
-        else if(a[mid]==2){
-            swap(a,mid,right);
-            right--;
-        }
-    }
-    }
-    public static void swap (int a[], int f ,int e){
-        int temp = a[f];
-        a[f]=a[e];
-        a[e]=temp;
+        Arrays.sort(a);
     }
 }
 
