e525528d5dda43988f883bb37477ae61e8c8193b
BHAVYA SHARMA <120469965+1192-BHAVYA-SHARMA@users.noreply.github.com>
2023-07-30 22:57:10 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.java
+++ Reverse a String - GFG/reverse-a-string.java
@@ -1,32 +1,53 @@
 //{ Driver Code Starts
-// Initial template for Java
+//Initial Template for Java
 
 import java.util.*;
 import java.io.*;
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S = read.readLine();
-            Solution ob = new Solution();
+import java.lang.*;
 
-            System.out.println(ob.revStr(S));
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
         }
     }
 }
 // } Driver Code Ends
 
 
-class Solution {
-    static String revStr(String S) {
-        // code here
-        String s="";
-        for(int i=S.length()-1;i>=0;i--){
-            char c =S.charAt(i);
-            s+=c;
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        // convert the string to character array
+        char[] charArray = str.toCharArray();
+        
+        int first = 0;
+        int last = charArray.length - 1;
+        
+        while(first<last){
+            // swap the elements
+            char temp = charArray[first];
+            charArray[first] = charArray[last];
+            charArray[last] = temp;
+            
+            first++;
+            last--;
         }
-        return s;
+        return new String(charArray);
+
     }
 }
